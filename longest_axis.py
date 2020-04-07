dcc565d152eddf4beb6fc291aacffbc86292222e
Nils wendt <nwendt@coxgpu01.rc.fas.harvard.edu>
2020-04-07 18:12:20 -0400

    cleaned up, calculates length and thickness. need to save lookuptable and pipeline all dendrites

--- longest_axis.py
+++ longest_axis.py
@@ -25,6 +25,11 @@ def load_graph(out_folder, bfs):
     return G
 
 def draw_graph(G, pos=None, save_name=''):
+    """
+    How to plot nx Graphs:
+    pos = draw_graph(G, save_name='G.png') # same pos for both
+    _ = draw_graph(SG, pos=pos, save_name='G_subgraph.png')
+    """
     if pos==None:
         pos = nx.spring_layout(G)
     nx.draw(G, pos, cmap = plt.get_cmap('jet'))
@@ -36,30 +41,9 @@ def draw_graph(G, pos=None, save_name=''):
     plt.show()
     return pos
 
-def longest_axis_exhaustive_alternative(G, return_extra=True):
-    """
-    Info: Search algorithm to find longest axis in a graph. 
-    Tries all combinations without any heuristics.
-    input: nx.Graph
-    output: Graph
-        optional: [float] length of longest path, [float] thickness of longest path
-    """
-    print('compute longest axis (exhaustive)')
-    longest_path = []
-    max_length = -1
-    for source in G.nodes:
-        for target in G.nodes:
-            try:
-                length = G[source][target]['weight']
-                if length > max_length:
-                    max_length = length
-                    longest_path = [source, target]
-            except: pass
-    
-    thickness =  G[longest_path[0]][longest_path[1]]['thick']
-    SG = G.subgraph(G[longest_path[0]][longest_path[1]]['path'])
-    if return_extra == True: return SG, max_length, thickness
-    else: return SG
+def longest_axis_dijkstra(G):
+#     Todo: implement with nx.dijkstra_path
+    pass
 
 def longest_axis_exhaustive(G):
     """
@@ -134,10 +118,14 @@ def extract_main_axis_from_skeleton(dendrite_id, dendrite_folder, seg_fn,
         else:
             WriteH5(dendrite_folder+'node_pos_longaxis2.h5', point_cloud)
 
-    return SG
+    return SG, endnodes
 
 def edge_length_and_thickness(G, node1, node2):
-    return G[node1][node2]['weight'], G[node1][node2]['thick'] 
+    length = nx.shortest_path_length(G, node1, node2, weight='weight')
+    # assumption for thickness: path is unique
+    thickness = nx.shortest_path_length(G, node1, node2, weight='thick')
+    thickness /= length
+    return length, thickness
     
 if __name__=='__main__':
 # if opt=='4': # longest graph path
@@ -148,11 +136,13 @@ if __name__=='__main__':
     res = [60,64,64] # z,y,x resolution of skeleton
     seg_fn = '/n/pfister_lab2/Lab/donglai/mito/db/30um_human/seg_64nm.h5'
     
-    G = extract_main_axis_from_skeleton(dendrite_id, dendrite_folder,
+    # get main axis:
+    G, nodeends = extract_main_axis_from_skeleton(dendrite_id, dendrite_folder,
                             seg_fn, res, write=True, shrink = False)
-    # display nx graph figures
-    display = False
-    if display == True:
-        pos = draw_graph(G, save_name='G_short.png')
-        _ = draw_graph(SG, pos=pos, save_name='G_short_cut.png')
+    
+    # get average thickness and length
+    length, thickness = edge_length_and_thickness(G, nodeends[0], nodeends[1])
+    
+    txt = [dendrite_id, thickness, length]
+    np.savetxt(txt, header = 'id, thickness, length')
     print('done')
